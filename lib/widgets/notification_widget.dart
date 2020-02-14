848a1ab4c184f73a0f2654e40debf682206e01ad
Wichayut Phongphanpanya <wichayut@Wichayuts-MacBook-Pro.local>
2020-02-14 20:13:05 +0700

    notification

--- lib/widgets/notification_widget.dart
+++ lib/widgets/notification_widget.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 
